6970d37cc6465100ec86625cbae6f8ec82b973b6
antran2704 <phamtrangiaan27@gmail.com>
2023-09-01 22:45:20 +0700

    update code

--- application/public/js/swiper.js
+++ application/public/js/swiper.js
@@ -1,11 +1,47 @@
-const swiper = new Swiper(".swiper", {
+// swiper timeline
+new Swiper(".swiper-timeline", {
   // Optional parameters
   loop: true,
-  slidesPerView: 3,
+  slidesPerView: 2,
   spaceBetween: 20,
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
     clickable: true
   },
+});
+
+new Swiper(".swiper-why", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+});
+
+// swiper other blog
+new Swiper(".swiper-blog", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 40,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+
+  breakpoints: {
+    992: {
+      slidesPerView: 3,
+      pagination: false
+    },
+    768: {
+      slidesPerView: 2,
+    }
+  }
 });
